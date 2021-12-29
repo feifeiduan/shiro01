d63d3faf030d17e989368356085daa241f8f108e
feifeiduan <946166823@qq.com>
2021-12-29 18:10:24 +0800

    Initial commit

---
+++ src/main/java/com/zking/demo/servlet.java
@@ -0,0 +1,50 @@
+package com.zking.demo;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/login.do")
+public class servlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        //获取到前端传过来的账号和密码
+        String username=req.getParameter("username");
+        String password=req.getParameter("password");
+        //创建用户登录令牌
+        UsernamePasswordToken token=new UsernamePasswordToken(
+                username,password
+        );
+        //获取用户主体信息
+        Subject subject = SecurityUtils.getSubject();
+        try {
+            subject.login(token);
+            resp.sendRedirect("main.jsp");
+        } catch (IncorrectCredentialsException e) {
+            session.setAttribute("msg","密码错误！");
+            resp.sendRedirect("login.jsp");
+        }catch (UnknownAccountException e){
+            session.setAttribute("msg","账号不存在！");
+            resp.sendRedirect("login.jsp");
+        }
+    }
+}
---
+++ src/main/java/shiro/MyRealm.java
@@ -0,0 +1,51 @@
+package shiro;
+
+import com.zking.model.User;
+import com.zking.service.IUserService;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+public class MyRealm extends AuthorizingRealm {
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+
+        String username = principalCollection.getPrimaryPrincipal().toString();
+        //获取用户的权限&角色信息
+        Set<String> roles = userService.getRole(username);
+        Set<String> permissions = userService.getPermission(username);
+        SimpleAuthorizationInfo info=new SimpleAuthorizationInfo();
+        info.setRoles(roles);
+        info.setStringPermissions(permissions);
+        //权限验证
+        return info;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        //身份验证
+        //getPrincipals身份信息
+        String password = authenticationToken.getCredentials().toString();
+        //getCredentials凭证
+        String username = authenticationToken.getPrincipal().toString();
+
+        User user = userService.selectByName(username);
+        SimpleAuthenticationInfo info=new SimpleAuthenticationInfo(
+                user.getUsername(),user.getPassword(), ByteSource.Util.bytes(user.getSalt()),this.getName()
+        );
+        return info;
+    }
+}
